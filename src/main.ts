772f8c49a55caca5585cd6cc5bd55f7409b0fda0
corymhall <43035978+corymhall@users.noreply.github.com>
2024-08-15 09:20:50 -0400

    switch to inline function for demo

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import * as pulumiCdk from '@pulumi/cdk';
 import { RemovalPolicy } from 'aws-cdk-lib';
 import { HttpApi, HttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
 import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class MyStack extends pulumiCdk.Stack {
@@ -15,9 +14,14 @@ export class MyStack extends pulumiCdk.Stack {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const handler = new NodejsFunction(this, 'handler', {
+    const handler = new Function(this, 'handler', {
+      handler: 'index.handler',
       runtime: Runtime.NODEJS_LATEST,
+      code: Code.fromInline(
+        'export default function handler() { return { statusCode: 200, body: "Hello World" };}',
+      ),
     });
+
     const api = new HttpApi(this, 'chall-Api', {});
     api.addRoutes({
       path: '/hello',
@@ -31,4 +35,3 @@ export class MyStack extends pulumiCdk.Stack {
 
 new MyStack('chall-cdk-test-app-dev');
 // new MyStack(app, 'cdk-test-app-prod', { env: prodEnv });
-
